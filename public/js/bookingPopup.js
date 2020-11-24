57cd5cc0df72ea28d891307a36cc9715bc78594c
gusjak <70512255+gusjak@users.noreply.github.com>
2020-11-24 16:18:29 +0100

    Changed link behaviour.
    Changed onclick="return false" from our HTML links to an e.preventDefault(); function in bookingPopup.js.

--- public/js/bookingPopup.js
+++ public/js/bookingPopup.js
@@ -1,21 +1,32 @@
 const modal = document.querySelector('.popup');
+const link = document.querySelector('.nav-bar a');
 
-window.addEventListener('keydown', function (event) {
-  if (event.key === 'Escape') {
+// Booking popup closes when the Escape key is pressed.
+window.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
     modal.style.display = 'none';
   }
 });
-
-window.onclick = function (event) {
-  if (event.target == modal) {
+// Booking popup closes when user clicks outside of the form.
+window.onclick = function (e) {
+  if (e.target == modal) {
     modal.style.display = 'none';
   }
 };
 
-document.getElementById('news-btn').addEventListener('click', () => {
+// Booking popup opens when user clicks on 'Booking'.
+document.getElementById('bookingBtn').addEventListener('click', (e) => {
+  e.preventDefault();
   modal.style.display = 'flex';
 });
 
-document.getElementById('close').addEventListener('click', () => {
+// Booking popup closes when user clicks on the X.
+document.getElementById('close').addEventListener('click', (e) => {
+  e.preventDefault();
   modal.style.display = 'none';
+});
+
+// Preventing the page scrolling back to top if Merch in the navigation bar is clicked.
+document.getElementById('merchBtn').addEventListener('click', (e) => {
+  e.preventDefault();
 });
