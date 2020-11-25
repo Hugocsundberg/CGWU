e81417dbdf63ae7719b6b0b45a398916f52b8bdb
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2020-11-25 14:08:45 +0100

    Added a delay for overlays

--- public/js/intersect.js
+++ public/js/intersect.js
@@ -1,13 +1,26 @@
 //Observer for adding animation class
 observerCallback = (entries) => {
     entries.forEach((entry) => {
-        if(entry.isIntersecting) {
+        if (entry.isIntersecting) {
             entry.target.classList.add('sectionActive')
+            try {
+                for (i = 0; i < entry.target.children.length; i++) {
+                    console.log(entry.target.children[i])
+                    if (entry.target.children[i].classList.contains('content-box-container')) {
+                        console.log(`this one contains the thing: ${entry.target.children[i]}`)
+                        entry.target.children[i].children[0].classList.add('delayFadeIn')
+                    }
+                }
+
+                entry.target.children
+            } catch (error) {
+                console.log(error)
+            }
         }
     })
-    }
+}
 
-observer = new IntersectionObserver(observerCallback)
+observer = new IntersectionObserver(observerCallback, { threshold: 0.00001 })
 document.querySelectorAll('section').forEach(section => {
     observer.observe(section)
 })
@@ -17,12 +30,11 @@ let isAtBottom = false
 let nextSection
 scrollArrowObserverCallback = (entries) => {
     entries.forEach((entry) => {
-        if(entry.isIntersecting) {
-            console.log(entry.target)
+        if (entry.isIntersecting) {
             //Sets next scroll point
             nextSection = entry.target.nextElementSibling
             //Sets 'isAtBottom to true or false'
-            if(entry.target.nextElementSibling.tagName !== 'SECTION') {
+            if (entry.target.nextElementSibling.tagName !== 'SECTION') {
                 scrollArrow.classList.add('scroll-arrow-bottom')
                 isAtBottom = true
             } else {
@@ -31,9 +43,9 @@ scrollArrowObserverCallback = (entries) => {
             }
         }
     })
-    }
+}
 
-scrollArrowObserver = new IntersectionObserver(scrollArrowObserverCallback, {threshold: 0.9})
+scrollArrowObserver = new IntersectionObserver(scrollArrowObserverCallback, { threshold: 0.9 })
 document.querySelectorAll('section').forEach(section => {
     scrollArrowObserver.observe(section)
 })
@@ -41,8 +53,8 @@ document.querySelectorAll('section').forEach(section => {
 scrollArrow = document.querySelector('.scroll-arrow')
 
 //Scrolls to next sctoll point or to top depending on 'isAtBottom' 
-scrollArrow.addEventListener('click', ()=>{
-    if(isAtBottom) {
+scrollArrow.addEventListener('click', () => {
+    if (isAtBottom) {
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
