1208274ab5c33149eacf51f56fea9ceb1b015c4c
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2020-11-25 16:05:15 +0100

    Fixed 'bug' where images shown under element

--- public/js/gridExpand.js
+++ public/js/gridExpand.js
@@ -2,24 +2,29 @@ const gridExpandButton = document.querySelector('section.grid-section .grid .exp
 const gridSection = document.querySelector('section.grid-section')
 
 let gridIsExpanded = false
-gridExpandButton.addEventListener('click', ()=>{
+gridExpandButton.addEventListener('click', () => {
     gridIsExpanded = gridIsExpanded ? false : true
     gridSection.classList.toggle('double-height')
-    if(gridIsExpanded) {
-        document.querySelector('section.grid-section .grid .expand button p').innerText="Less"
+    if (gridIsExpanded) {
+        console.log(document.querySelectorAll('showOnLoad'))
+        document.querySelectorAll('.showOnLoad').forEach((element) => {
+            console.log(element)
+            element.classList.remove('opacity0')
+        })
+        document.querySelector('section.grid-section .grid .expand button p').innerText = "Less"
         window.scrollTo({
             top: document.querySelector('section.grid-section').offsetTop + window.innerHeight,
             behavior: 'smooth'
         })
         document.querySelector('section.grid-section .grid .expand button img').classList.add('scroll-arrow-bottom')
     } else {
-        document.querySelector('section.grid-section .grid .expand button p').innerText="More"
+        document.querySelector('section.grid-section .grid .expand button p').innerText = "More"
         window.scrollTo({
             top: document.querySelector('section.grid-section').offsetTop,
             behavior: 'smooth'
         })
         document.querySelector('section.grid-section .grid .expand button img').classList.remove('scroll-arrow-bottom')
     }
-}) 
+})
 
 console.log(document.querySelector('section.grid-section').offsetTop)
