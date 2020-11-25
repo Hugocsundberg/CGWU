003d405337c60a706089eceb110cf22ac464ba36
gusjak <70512255+gusjak@users.noreply.github.com>
2020-11-25 18:19:42 +0100

    Added final images
    Added the final images to the project. Will have to optimize them later, though.

--- public/js/colorPicker.js
+++ public/js/colorPicker.js
@@ -1,54 +1,14 @@
-// // Array for mobile backgrounds and its respective color button.
-// const mobile = [
-//   {
-//     path: 'images/bg/color/mobile/',
-//     src: 'Mobile-5-blue.jpg',
-//   },
-//   {
-//     path: 'images/bg/color/mobile/',
-//     src: 'Mobile-5-red.jpg',
-//   },
-//   {
-//     path: 'images/bg/color/mobile/',
-//     src: 'Mobile-5-yellow.jpg',
-//   },
-//   {
-//     path: 'images/bg/color/mobile/',
-//     src: 'Mobile-5-white.jpg',
-//   },
-// ];
-
-// // Array for desktop backgrounds and its respective color button.
-// const desktop = [
-//   {
-//     path: 'images/bg/color/desktop/',
-//     src: 'Desktop-5-blue.jpg',
-//   },
-//   {
-//     path: 'images/bg/color/desktop/',
-//     src: 'Desktop-5-red.jpg',
-//   },
-//   {
-//     path: 'images/bg/color/desktop/',
-//     src: 'Desktop-5-yellow.jpg',
-//   },
-//   {
-//     path: 'images/bg/color/desktop/',
-//     src: 'Desktop-5-white.jpg',
-//   },
-// ];
-
 const colorButtons = document.querySelectorAll('.color-buttons label .button');
 colorButtons.forEach((button) => {
   button.addEventListener('click', (e) => {
-    if (e.target.parentElement.parentElement.classList.contains('white')) {
-      document.querySelector('section.section-4 .bg-image img').src = '/public/images/bg/color/desktop/Studio_bil_vit.png';
-    } else if (e.target.parentElement.parentElement.classList.contains('yellow')) {
-      document.querySelector('section.section-4 .bg-image img').src = '/public/images/bg/color/desktop/Studio_bil_gul.png';
+    if (e.target.parentElement.parentElement.classList.contains('gray')) {
+      document.querySelector('section.section-4 .bg-image img').src = '/public/images/bg/color/Studio-gray.png';
+    } else if (e.target.parentElement.parentElement.classList.contains('green')) {
+      document.querySelector('section.section-4 .bg-image img').src = '/public/images/bg/color/Studio-green.png';
     } else if (e.target.parentElement.parentElement.classList.contains('red')) {
-      document.querySelector('section.section-4 .bg-image img').src = '/public/images/bg/color/desktop/Studio_bil_röd.png';
+      document.querySelector('section.section-4 .bg-image img').src = '/public/images/bg/color/Studio-red.png';
     } else if (e.target.parentElement.parentElement.classList.contains('blue')) {
-      document.querySelector('section.section-4 .bg-image img').src = '/public/images/bg/color/desktop/Studio_bil_blå.png';
+      document.querySelector('section.section-4 .bg-image img').src = '/public/images/bg/color/Studio-blue.png';
     }
   });
 });
