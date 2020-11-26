dc88a532c0c2622c36cbfe1ad25609cb615ba481
gusjak <70512255+gusjak@users.noreply.github.com>
2020-11-26 19:04:41 +0100

    Image Optimization
    Swapped every .png image to a .webp version.
    
    Dropped about 800MB of size.

--- public/js/colorPicker.js
+++ public/js/colorPicker.js
@@ -2,13 +2,13 @@ const colorButtons = document.querySelectorAll('.color-buttons label .button');
 colorButtons.forEach((button) => {
   button.addEventListener('click', (e) => {
     if (e.target.parentElement.parentElement.classList.contains('gray')) {
-      document.querySelector('section.section-4 .bg-image img').src = 'images/bg/color/Studio-gray.png';
+      document.querySelector('section.section-4 .bg-image img').src = 'public/images/bg/color/Studio-gray.webp';
     } else if (e.target.parentElement.parentElement.classList.contains('green')) {
-      document.querySelector('section.section-4 .bg-image img').src = 'images/bg/color/Studio-green.png';
+      document.querySelector('section.section-4 .bg-image img').src = 'public/images/bg/color/Studio-green.webp';
     } else if (e.target.parentElement.parentElement.classList.contains('red')) {
-      document.querySelector('section.section-4 .bg-image img').src = 'images/bg/color/Studio-red.png';
+      document.querySelector('section.section-4 .bg-image img').src = 'public/images/bg/color/Studio-red.webp';
     } else if (e.target.parentElement.parentElement.classList.contains('blue')) {
-      document.querySelector('section.section-4 .bg-image img').src = 'images/bg/color/Studio-blue.png';
+      document.querySelector('section.section-4 .bg-image img').src = 'public/images/bg/color/Studio-blue.webp';
     }
   });
 });
